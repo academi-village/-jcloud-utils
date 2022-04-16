7851209b7ba92c10e490378db0784431e9bbe399
Younes Rahimi <y.rahimi@jibit.ir>
2022-04-16 15:32:24 +0430

    Add ExceptionUtil

---
+++ src/main/java/com/github/academivillage/jcloud/util/ExceptionUtil.java
@@ -0,0 +1,40 @@
+package com.github.academivillage.jcloud.util;
+
+import com.github.academivillage.jcloud.errors.AppError;
+import com.github.academivillage.jcloud.errors.AppException;
+import lombok.SneakyThrows;
+
+public class ExceptionUtil {
+
+    public static <T> T TODO() {throw new TodoException();}
+
+    @SneakyThrows
+    public static <T> T rethrow(Throwable throwable) {throw throwable;}
+
+    public static Throwable getRootCause(Throwable original) {
+        Throwable rootCause = original;
+        Throwable cause     = original.getCause();
+        while (cause != null && cause != rootCause) {
+            rootCause = cause;
+            cause     = cause.getCause();
+        }
+        return rootCause;
+    }
+
+    private static class TodoException extends AppException {
+
+        private static final AppError API_NOT_IMPLEMENTED_ERROR = new AppError() {
+
+            @Override
+            public String getCode() {return "api.not_implemented";}
+
+            @Override
+            public int getHttpStatusCode() {return 500;}
+
+            @Override
+            public String getMessage() {return "API is not implemented yet.";}
+        };
+
+        private TodoException() {super(API_NOT_IMPLEMENTED_ERROR);}
+    }
+}
